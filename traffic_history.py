cf3408a2d07511a855267c5a4d0a030e4d53c24f
Bijiabo <bijiabo@gmail.com>
2017-12-10 01:03:42 +0800

    Add: 实现绘制单个设备网络使用图片

--- traffic_history.py
+++ traffic_history.py
@@ -4,14 +4,24 @@ import sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+import json
+
+# 读取 mac 地址配置
+def load_device_name_config(): 
+    device_name_config_file = open('./device_name.json', 'r')
+    device_name_config_file_content = device_name_config_file.read()
+    device_name_config = json.loads(device_name_config_file_content)
+    print(device_name_config)
+    return device_name_config
+device_name_config = load_device_name_config()
 
 # 绘图方法
 def add_data_to_image(mac, data_array):
     file_name = re.sub(r'\:', '_', mac)
-    
+
     # plt.figure(figsize=(20,10))
     # plt.plot([1,2,3,4,5,6])
-    plt.plot(np.array(data_array), label=mac)
+    plt.plot(np.array(data_array), label=device_name_config.get(mac, mac))
     # plt.legend()
     plt.legend(bbox_to_anchor=(1.04,1), borderaxespad=0)
     plt.subplots_adjust(right=0.8)
@@ -26,6 +36,21 @@ def save_image():
     fig.set_size_inches(12, 5)
     plt.draw()
     fig.savefig('./images/all.png')#, dpi=100)
+
+def draw_single_device_traffic(mac, up_array, down_array):
+    file_name = re.sub(r'\:', '_', mac)
+    plt.gcf().clear()
+    plt.plot(np.array(up_array), label='UP')
+    plt.plot(np.array(down_array), label='DOWN')
+    plt.legend(bbox_to_anchor=(1.04,1), borderaxespad=0)
+    plt.subplots_adjust(right=0.8)
+    plt.ylabel('network KB/s')  #为y轴加注释
+    plt.xlabel('time point')
+    plt.title('Network Traffic ' + device_name_config.get(mac, mac))
+    fig = plt.gcf()
+    # fig.set_size_inches(12, 5)
+    plt.draw()
+    fig.savefig('./images/{file_name}.png'.format(file_name=file_name))
 
 conn = sqlite3.connect('test.db')
 print('Opened database successfully')
@@ -63,12 +88,16 @@ for key in recent_devices_traffic_data.keys():
         recent_devices_traffic_data[key]['UP'].append(up)
         recent_devices_traffic_data[key]['DOWN'].append(down)
     add_data_to_image(key, recent_devices_traffic_data[key]['DOWN'])
+save_image()
+
+for key in recent_devices_traffic_data.keys():
+    draw_single_device_traffic(key, recent_devices_traffic_data[key]['UP'], recent_devices_traffic_data[key]['DOWN'])
 
 print(recent_devices_traffic_data)
 
 # 绘制并保存图形
 print('draw image...')
-save_image()
+
 print('save image done.')
 
 print("Done.")
